78b1a9d97efe4dea7b068185676edcfadf902c85
Miro Renda <Miroslav.Renda@tesena.com>
2024-12-20 21:06:22 +0100

    Zmena 5 radku v test branche

--- code.js
+++ code.js
@@ -2,4 +2,4 @@ console.log("neco");
 console.log("další řádek změn");
 console.log("Třetí řádek změn");
 // Znovu komentář + rozšíření
-// Toto je modifikované v Sprint1 branch
+// Toto je modifikované v Sprint1 branch _MENIM V TEST 5 RADEK
